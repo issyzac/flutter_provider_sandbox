c38680bc26b25b28995744bb9223fcfdd06ab787
issyzac <issyzac.iz@gmail.com>
2020-06-08 10:29:41 +0300

    Registered the webapi service in service locator and added fake data

--- lib/services/service_locator.dart
+++ lib/services/service_locator.dart
@@ -29,19 +29,23 @@
  */
 
 import 'package:get_it/get_it.dart';
+import 'package:moolax/services/web_api/web_api_fake.dart';
 import 'currency/currency_service.dart';
 import 'currency/currency_service_fake.dart';
+import 'currency/currency_service_implementation.dart';
 import 'storage/storage_service.dart';
 import 'storage/storage_service_implementation.dart';
 import '../business_logic/view_models/calculate_screen_viewmodel.dart';
 import '../business_logic/view_models/choose_favorites_viewmodel.dart';
+import 'web_api/web_api.dart';
 
 GetIt serviceLocator = GetIt.instance;
 
 void setupServiceLocator() {
 
   serviceLocator.registerLazySingleton<StorageService>(() => StorageServiceImpl());
-  serviceLocator.registerLazySingleton<CurrencyService>(() => CurrencyServiceFake());
+  serviceLocator.registerLazySingleton<CurrencyService>(() => CurrencyServiceImpl());
+  serviceLocator.registerLazySingleton<WebApi>(() => FakeWebApi());
 
   serviceLocator.registerFactory<CalculateScreenViewModel>(() => CalculateScreenViewModel());
   serviceLocator.registerFactory<ChooseFavoritesViewModel>(() => ChooseFavoritesViewModel());
